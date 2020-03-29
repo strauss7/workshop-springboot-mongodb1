de342e1b1f86e21898e06f0c7d39f26e209519b1
strauss7 <wbsabbyyuji@gmail.com>
2020-03-29 15:39:59 -0300

    Operacacao de instanciacao de base de dados

---
+++ src/main/java/com/amira/workshopmongodb1/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.amira.workshopmongodb1.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.amira.workshopmongodb1.domain.User;
+import com.amira.workshopmongodb1.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown","maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
