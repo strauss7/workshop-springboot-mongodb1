ffd84b8787b4dcf2c9f4de56b6aa1a8f66198bd7
strauss7 <wbsabbyyuji@gmail.com>
2020-03-29 14:07:14 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/amira/workshopmongodb1/Workshopmongodb1Application.java
@@ -0,0 +1,13 @@
+package com.amira.workshopmongodb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshopmongodb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshopmongodb1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/amira/workshopmongodb1/Workshopmongodb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amira.workshopmongodb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshopmongodb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
