5ed5bc024f8fedd8d7fee3a4059f02efcd8c1eab
strauss7 <wbsabbyyuji@gmail.com>
2020-03-30 14:06:33 -0300

    Consulta simples com @Query

--- src/main/java/com/amira/workshopmongodb1/repository/PostRepository.java
+++ src/main/java/com/amira/workshopmongodb1/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.amira.workshopmongodb1.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.amira.workshopmongodb1.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
