45dded775472940b9afd41d6916eab489e82ad5b
strauss7 <wbsabbyyuji@gmail.com>
2020-03-30 00:34:39 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/amira/workshopmongodb1/config/Instantiation.java
+++ src/main/java/com/amira/workshopmongodb1/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.amira.workshopmongodb1.domain.Post;
 import com.amira.workshopmongodb1.domain.User;
+import com.amira.workshopmongodb1.dto.AuthorDTO;
 import com.amira.workshopmongodb1.repository.PostRepository;
 import com.amira.workshopmongodb1.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para SP, abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para SP, abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
